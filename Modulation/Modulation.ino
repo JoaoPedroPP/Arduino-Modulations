2600ddeff222918584b48fe5cfe845d4856b1137
joao-poloni <joao.poloni@ibm.com>
2018-11-22 20:15:06 -0200

    Added psk library to Modulation.ino

--- Modulation/Modulation.ino
+++ Modulation/Modulation.ino
@@ -1,19 +1,23 @@
 #include "ask.hpp"
 #include "fsk.hpp"
+#include "psk.hpp"
 
 FSKModulation fsk;
 ASKModulation ask;
+PSKModulation psk;
 
 int led = 13;
 int ledPin = 9;
 
 void setup() {
-  ask.begin();
+  // ask.begin();
 //  fsk.begin();
+    psk.begin();
 }
 
 void loop() {
-  ask.transmite('U');
+  psk.transmite('U');
+  // ask.transmite('U');
 //    fsk.transmite('U',1);
 //  fsk.transmite("joao pedro poloni ponce escreveu esse software", 46);
 }
